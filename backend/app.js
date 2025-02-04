3450b517a18620413ab5010221a9f5be9f5d7e0e
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2025-02-04 04:17:16 +0100

    webhook verification

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,6 @@ const morgan = require('morgan');
 
 const bodyParser = require('body-parser');
 
-const whatsappRouter = require('./src/routes/whatsapp.route');
-
 const api = require('./src/routes/api');
 
 const app = express();
@@ -27,13 +25,6 @@ app.use(
     extended: true,
   })
 );
-
-//const url ='https://vss.camtrack.net:36301/fileSrv/fileDown.php?filePath=QzovUHJvZ3JhbSBGaWxlcyAoeDg2KS9Wc3NTZXJ2aWNlL3N0b3JhZ2UvYmluL3g2NC8uLi8uLi8uLi9odGRvY3MvdnNzRmlsZXMvYWxhcm1SZWNvcmQvMjAyNF8wNV8xMi9WT0tTMDAxMy9jaDAzXzIwMjQwNTEyXzA4MzkyOF8wODM5NDNfMTIyLmh3&token=40a7fbeb380a435c6e7f2053f070af16'
-
-//const downloadPath = path.resolve(`./public/assets/video/${id}.mp4`)
-
-// Accepts POST requests at /webhook endpoint
-app.use('/webhook', whatsappRouter);
 
 app.use('/api', api);
 
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,11 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const ymaneRouter= require('./ymane.route')
+const ymaneRouter = require('./ymane.route');
 
 const api = express();
 
 api.use('/ymanebot', ymaneRouter);
 
-
 module.exports = api;
- 
